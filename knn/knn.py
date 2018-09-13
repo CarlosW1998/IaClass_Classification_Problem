99034f38fa071cabd7cfd7740024569b67aa513c
CarlosW1998 <carlos-walte@hotmail.com>
2018-09-12 21:58:23 -0300

    finalizado

--- knn/knn.py
+++ knn/knn.py
@@ -9,21 +9,14 @@ class knn:
         dist = dist**(1/2)
         return dist
 
-    def  ppush(self, mylist, newcase):
-        if len(mylist) < self.nsize:
-            mylist.append(newcase)
-            return
-        if newcase[0] < mylist[self.nsize-1][0]:
-            mylist[self.nsize-1] = newcase
-            mylist.sort()
-            return
     def classify(self, case):
         neighboors = []
         for a in self.data:
-            self.ppush(neighboors, [self.dist(case, a['Data']), a['Type']])
+            neighboors.append([self.dist(case, a['Data']), a['Type']])
         counter = {}
-        for a in neighboors:
-            if a[1]  not in counter.keys():
+        neighboors.sort()
+        for a in neighboors[:self.nsize]:
+            if a[1] not in counter.keys():
                 counter[a[1]] = 0;
             counter[a[1]] += 1
         key = ''
